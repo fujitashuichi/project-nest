7a061f850d5e69be1bd7649e6651d86ab86f3fb4
fujitashuichi <fujitashuu14@gmail.com>
2026-03-01 14:25:13 +0900

    Fix: devコマンドを修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    const test = async () => {
-      const data = await fetch("http://localhost:30000/api/users");
-      console.log(await data.json())
-    }
-    test();
-  }, []);
-
   return (
     <>
     </>
