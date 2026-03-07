c5710ad48c55a678055d40b6a19dd072b011399f
fujitashuichi <fujitashuu14@gmail.com>
2026-03-07 11:57:18 +0900

    Tested: errorHandlerの動作確認

---
+++ node_modules/backend/src/__test__/grobalErrorHandler.test.ts
@@ -0,0 +1,80 @@
+import { Response } from "express";
+import { afterEach, beforeEach, describe, expect, it, MockInstance, vi } from "vitest";
+import { createResponseMock } from "../__mock__/index.js";
+import { globalErrorHandler } from "../middleware/index.js";
+import { ConfirmPasswordError, EmailAlreadyRegisteredError, InvalidPasswordError, UserAuthError, UserUndefinedError } from "../error/UserAuthError.js";
+import { DuplicateProjectError, ProjectError } from "../error/ProjectError.js";
+
+describe("globalErrorHandlerが正しく機能する", () => {
+  const _req = vi.fn();
+  const _next = vi.fn();
+  let res: Response | null = null;
+  let err: Error | null = null;
+  let consoleErrorMock: MockInstance;
+  beforeEach(() => {
+    res = createResponseMock();
+    consoleErrorMock = vi.spyOn(console, 'error').mockImplementation(() => {});
+  });
+  afterEach(() => {
+    res = null;
+    err = null;
+    consoleErrorMock.mockRestore();
+  });
+
+  it("UserUndefinedError", () => {
+    err = new UserUndefinedError();
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(404);
+  });
+
+  // register
+  it("EmailAlreadyRegisteredError", () => {
+    err = new EmailAlreadyRegisteredError("example@email.com");
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(409);
+  });
+  it("InvalidPasswordError", () => {
+    err = new InvalidPasswordError();
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(400);
+  });
+
+  // login
+  it("ConfirmPasswordError", () => {
+    err = new ConfirmPasswordError();
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(401);
+  });
+
+  // other
+  it("UserAuthError", () => {
+    err = new UserAuthError("Any Error Massage");
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(400);
+  });
+
+  // postProduct
+  it("DuplicateProjectError", () => {
+    err = new DuplicateProjectError();
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(400);
+  });
+
+  // other
+  it("OtherUserAuthError", () => {
+    err = new UserAuthError("Any Message");
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(400);
+  });
+  it("OtherProjectError", () => {
+    err = new ProjectError("Any Message");
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(400);
+  });
+
+  it("Other Uncaught Error", () => {
+    err = new Error("Normal Error");
+    globalErrorHandler(err as any, _req as any, res!, _next);
+    expect(res!.status).toBeCalledWith(500);
+  });
+})
---
+++ packages/backend/src/error/ProjectError.ts
@@ -0,0 +1,13 @@
+export class ProjectError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ProjectError";
+  }
+}
+
+export class DuplicateProjectError extends ProjectError {
+  constructor() {
+    super("Cannot create Project because same ProjectName found");
+    this.name = "ProjectAlreadyCreatedError";
+  }
+}
