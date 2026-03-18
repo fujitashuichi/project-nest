ca20a8e4732b4cbc0add101afd3d3694e82295ec
fujitashuichi <fujitashuu14@gmail.com>
2026-03-18 16:42:35 +0900

    feature(FE): UserPageの骨格を作成

--- packages/frontend/src/app/pages/UserPage.tsx
+++ packages/frontend/src/app/pages/UserPage.tsx
@@ -1,8 +1,21 @@
+import { useAuth } from "../../Context";
+import { LoginContainer } from "../../features/auth/components";
 
 export function UserPage() {
-  return (
-    <div>
-      userPage
-    </div>
-  )
+  const { session } = useAuth();
+  const { status: sessionStatus } = session;
+
+
+  return (<div>
+    {sessionStatus === "idle" &&
+      <div>
+        <h1>ログインしていません</h1>
+        <LoginContainer />
+      </div>
+    }
+    {sessionStatus === "active" &&
+      <h1>ユーザー情報</h1>
+    }
+
+  </div>)
 }
