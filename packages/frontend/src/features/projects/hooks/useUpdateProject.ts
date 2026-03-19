bdc12adc6a4ce7a6aa726f75ab7f5152c785e05a
fujitashuichi <fujitashuu14@gmail.com>
2026-03-19 15:47:21 +0900

    change(hooks): useUpdateProjectを差し替え

--- packages/frontend/src/features/projects/hooks/useUpdateProject.ts
+++ packages/frontend/src/features/projects/hooks/useUpdateProject.ts
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { parseFormData } from "../../../lib";
 import { PostProjectRequestSchema } from "@pkg/shared";
 import { updateProject } from "../api";
-import type { Project } from "@pkg/shared";
+import type { PostProjectRequest, Project } from "@pkg/shared";
 import type { ProjectCtxType } from "../../../Context";
+import { useMutation } from "@tanstack/react-query";
 
 
 const errorMap = {
@@ -16,36 +17,35 @@ const errorMap = {
 
 type Result = ProjectCtxType["update"];
 
+
 export const useUpdateProjects = (reload: ProjectCtxType["getProjects"]["get"]): Result => {
-  const [status, setStatus] = useState<Result["status"]>("idle");
   const [errorMessage, setErrorMessage] = useState<Result["errorMessage"]>(null);
+
+  const mutation = useMutation({
+    mutationFn: ({ project, id }: { project: PostProjectRequest, id: Project["id"] }) => updateProject(project, id),
+    onSuccess: async (result) => {
+      if (!result.success) return setErrorMessage(errorMap[result.errorType]);
+
+      await reload(); // 楽観更新をする際はここを差し替え
+    }
+  });
 
   const update = async (e: React.SubmitEvent<HTMLFormElement>, id: Project["id"]) => {
     e.preventDefault();
 
-    setStatus("loading");
-
     const formData: FormData = new FormData(e.currentTarget);
     const parsed = await parseFormData(formData, PostProjectRequestSchema);
 
-    if (!parsed.success) {
-      setStatus("idle");
-      alert("入力内容に不備があります");
-      return;
-    }
+    if (!parsed.success) return alert("入力内容に不備があります");
 
-    const result = await updateProject(parsed.data, id);
-    if (!result.success) {
-      setErrorMessage(errorMap[result.errorType]);
-      return setStatus("error");
-    }
-
-    await reload(); // 楽観更新をする際はここを差し替え
-    setStatus("success");
+    mutation.mutate({ project: parsed.data, id });
   }
 
-  const reset = () => setStatus("idle");
 
-
-  return { update, status, errorMessage, reset };
+  return {
+    update,
+    reset: mutation.reset,
+    status: mutation.status,
+    errorMessage
+  };
 }
