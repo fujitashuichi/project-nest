67a0dc6f7d7305a7e7a13b73518f113e85e72d9c
fujitashuichi <fujitashuu14@gmail.com>
2026-03-24 12:54:22 +0900

    fix(BE): prismaClientのインポート先を修正

--- packages/backend/src/lib/prisma.ts
+++ packages/backend/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { ENV } from "../config/env.js";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../../generated/prisma/index.js";
 
 export const prisma = new PrismaClient({
   adapter: new PrismaPg({ connectionString: ENV.DATABASE_URL })
