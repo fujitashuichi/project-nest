63b0cd0c059b96938155ca1f8d5385d352a9c776
fujitashuichi <fujitashuu14@gmail.com>
2026-04-15 14:01:46 +0900

    refactor(BE/router/auth/v2): 既存のsessionResponse型に統合

--- packages/backend/src/middleware/authorize_v2.ts
+++ packages/backend/src/middleware/authorize_v2.ts
@@ -1,10 +1,11 @@
 import Credentials from "@auth/express/providers/credentials";
 import { UserService } from "../service/user.service.js";
 import { verifyToken } from "../lib/jwt.js";
-import { UnAuthorizedError } from "../error/AuthError.js";
+import { AuthError, UnAuthorizedError } from "../error/AuthError.js";
 import { ExpressAuthConfig } from "@auth/express";
 import { UserUndefinedError } from "../error/UserError.js";
 import { JWTPayload } from "../types/types.payload.js";
+import { SessionResponse } from "@pkg/shared";
 
 
 const service = new UserService();
@@ -33,15 +34,20 @@ export const authConfig: ExpressAuthConfig = {
   callbacks: {
     jwt: ({ token, user }) => {
       if (user?.email) {
+        if (!user.id) throw new AuthError("user.id undefined", "AuthError");
+
         token.email = user.email;
+        token.sub = user.id;
       }
       return token;
     },
     session: ({ session, token }) => {
       if (session.user) {
-        if (!token.email) throw new Error("token.sub undefined");
+        if (!token.sub) throw new AuthError("token.sub undefined", "AuthError");
+        if (!token.email) throw new AuthError("token.email undefined", "AuthError");
 
-        session.user.email = token.email;
+        session.user.id = token.sub;
+        session.user.email = token.email
       }
 
       return session;
