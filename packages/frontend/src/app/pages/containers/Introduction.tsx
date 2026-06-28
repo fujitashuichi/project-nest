bcfe34dbc46186f9f6f59786698c0df4e9ff563d
fujitashuichi <fujitashuu14@gmail.com>
2026-06-29 03:04:04 +0900

    style: UIの調整

--- packages/frontend/src/app/pages/containers/Introduction.tsx
+++ packages/frontend/src/app/pages/containers/Introduction.tsx
@@ -5,32 +5,47 @@ function Introduction() {
   return (<>
     <div className="min-h-[80vh] flex flex-col items-center justify-center px-6 text-center space-y-12 animate-in fade-in slide-in-from-bottom-8 duration-1000">
       {/* メインビジュアル / キャッチコピー */}
-      <div className="space-y-6 max-w-2xl">
-        <div className="inline-flex items-center gap-2 px-3 py-1 rounded-full bg-blue-50 border border-blue-100 text-blue-600 text-xs font-bold tracking-widest uppercase">
-          Make UI Simple
+      <div className="space-y-6 max-w-2xl pt-6">
+        <div className="inline-flex items-center gap-2 px-3 py-1 mb-6 rounded-full bg-blue-50 border border-blue-100 text-blue-600 text-xs font-bold">
+          TypeScript学習者によるポートフォリオ
         </div>
-        <h1 className="text-4xl md:text-6xl font-extrabold text-slate-900 tracking-tighter leading-[1.1]">
-          ボトムアップで<br />
-          <span className="text-transparent bg-clip-text bg-linear-to-r from-blue-600 to-indigo-500">
-            泥臭さを糧に
-          </span>
-        </h1>
+
+        <ul className="flex flex-col gap-y-4 max-w-10/12 text-left">
+          <li>
+            <h2 className="text-4xl">BEへの挑戦</h2>
+            <p className="text-2xs">Expressの基本パターンを学習</p>
+          </li>
+          <li>
+            <h2 className="text-4xl">フルスタックへの挑戦</h2>
+            <p className="text-2xs">
+              FEからBEまで動くものを作ってみたいと思い、挑戦しました。
+            </p>
+          </li>
+        </ul>
+
         <div className="text-lg text-slate-500 max-w-lg mx-auto leading-relaxed">
-          <p>多層防御に基づいたBEへの挑戦</p>
-          <p>状態管理を考えたFEの復習</p>
+          <p>
+            UIデザインは学習範囲外のため、AI生成を用いています
+          </p>
         </div>
       </div>
 
-      {/* ログインボタン配置エリア */}
+      {/* リンク */}
       <div className="flex flex-col items-center gap-4">
         <Link to="/login">
           <AppButton variant="primary" className="w-auto">
             Login / SignUp
           </AppButton>
         </Link>
-        <p className="text-xs text-slate-400 font-medium">
-          アカウントをお持ちでない場合も、こちらから開始できます
-        </p>
+
+        <a
+          href={"https://github.com/fujitashuichi/shared-driven-layered-ddd"}
+          target="_blank" rel="noreferrer noopener"
+        >
+          <AppButton variant="primary" className="w-fit">
+            ❐ GitHub
+          </AppButton>
+        </a>
       </div>
 
       {/* 装飾 */}
